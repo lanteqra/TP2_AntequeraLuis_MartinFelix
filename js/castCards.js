4ed1f6af7fc2d0a817adf322d26f16e71d54277c
lanteqra <lante89@gmail.com>
2026-07-09 19:32:11 -0400

    Formatage des fichiers et suppression de main.js (non utilisé)

--- js/castCards.js
+++ js/castCards.js
@@ -1,21 +1,21 @@
 gsap.registerPlugin(ScrollTrigger);
 
-    const cards = gsap.utils.toArray(".distribution__item");
+const cards = gsap.utils.toArray(".distribution__item");
 
-    gsap.fromTo(cards,
-        {
-            y: 100,
-            opacity: 0
-        },
-        {
-            y: 0,
-            opacity: 1,
-            duration: 5,
-            ease: "elastic.out(1, 0.5)",
-            stagger: 0.15,
-            scrollTrigger: {
-                trigger: "#distribution",
-                start: "top 45%",
-            }
+gsap.fromTo(cards,
+    {
+        y: 100,
+        opacity: 0
+    },
+    {
+        y: 0,
+        opacity: 1,
+        duration: 5,
+        ease: "elastic.out(1, 0.5)",
+        stagger: 0.15,
+        scrollTrigger: {
+            trigger: "#distribution",
+            start: "top 45%",
         }
-    );
+    }
+);
